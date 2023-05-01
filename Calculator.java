5cfce1a5a495ab5f0f1b6a1a1a9bc2e81b65a340
seoweed <darkweed1106@gmail.com>
2023-05-01 12:49:00 +0900

    create basic java project

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a, b, c, d, e, f;
+        a = 12;
+        b = 2;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
---
+++ Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        int kor = 77;
+        int eng = 87;
+        int sum = kor + eng;
+
+        System.out.println("총점 = " + sum);
+        System.out.println("평균 = " + (sum / 2));
+
+    }
+}
