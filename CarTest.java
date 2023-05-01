bbc8483ae7280dd5f6cf2d4876e7726cbeac88e9
seoweed <darkweed1106@gmail.com>
2023-05-01 13:45:34 +0900

    변수 실습

---
+++ CarTest.java
@@ -0,0 +1,22 @@
+public class CarTest {
+    public static void main(String[] args) {
+
+
+        String model = "BMW528i";
+        System.out.println("model = " + model);
+        long distance = 100000L;
+        System.out.println("distance = " + distance);
+        int price = 90000000;
+        System.out.println("price = " + price);
+        String company = "BMW";
+        System.out.println("company = " + company);
+        char type = 'A';
+        System.out.println("type = " + type);
+        boolean auto = true;
+        System.out.println("auto = " + auto);
+        short year = 2010;
+        System.out.println("year = " + year);
+        float gasmi = 12.5f;
+        System.out.println("gasmi = " + gasmi);
+    }
+}
