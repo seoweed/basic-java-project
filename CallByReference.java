33c1289199bb37a194abad5ffbfdba58b30bf34d
seoweed <darkweed1106@gmail.com>
2023-05-03 20:27:48 +0900

    create method

---
+++ CallByReference.java
@@ -0,0 +1,17 @@
+public class CallByReference {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int i = addArray(a);
+        System.out.println("i = " + i);
+
+    }
+
+
+    public static int addArray(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
---
+++ MethodMakeTest.java
@@ -0,0 +1,16 @@
+public class MethodMakeTest {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int i = add(a, b);
+        System.out.println("i = " + i);
+
+
+    }
+    public static int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+
+}
